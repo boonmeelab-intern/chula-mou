9734d8fae3b8542fd0a67e71e3b945190c9a5473
anyafleur <anyay.ks@gmail.com>
2019-07-01 15:00:31 +0700

    Add new json

--- data/excel-to-json.js
+++ data/excel-to-json.js
@@ -25,7 +25,62 @@ const result = excelToJson({
   }
 });
 
-var jsonData = JSON.stringify(result);
+var mou = {
+  code: "",
+  country: "",
+  uniOrg: {
+    name: "",
+    abbreviation: "",
+    otherName: ""
+  },
+  facDep: "",
+  date: {
+    signing: 0,
+    expiry: 0,
+    updated: 0
+  },
+  cooperation: {
+    level: "",
+    type: ""
+  },
+  note: ""
+};
+
+var newResult = { MOU: [] };
+
+for (var n in result.MOU) {
+  var i = result.MOU[n];
+  if (i.code && i.country) {
+    console.log("if");
+    var tmp = mou;
+
+    tmp.code = i.code;
+    tmp.country = i.country;
+    tmp.uniOrg = {
+      name: i.uniOrg,
+      abbreviation: i.abbreviation || "",
+      otherName: i.otherName || ""
+    };
+    tmp.facDep = i.facDep || "";
+    tmp.date = {
+      signing: i.signing || 0,
+      expiry: i.expiry || 0,
+      updated: i.updated || 0
+    };
+    tmp.cooperation = {
+      level: i.level || "",
+      type: i.type || ""
+    };
+    tmp.note = i.note || "";
+
+    newResult.MOU.push(tmp);
+  }
+}
+
+console.log(newResult);
+
+// var jsonData = JSON.stringify(result);
+var jsonData = JSON.stringify(newResult);
 
 fs.writeFile(outputFile, jsonData, function(err) {
   if (err) {
